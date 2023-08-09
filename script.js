e17a2acf0ca78e4d808cde85dbca018f57f5ad8b
Lisa Miner <midnightmoet@gmail.com>
2023-08-09 17:20:55 -0400

    Code push

--- script.js
+++ script.js
@@ -8,21 +8,33 @@ const imgsElement = document.querySelectorAll("img");
 const imageContainerElement = document.querySelector(".image-container");
 
 let currentImg = 1;
+let timeout;
 
 nextElement.addEventListener("click", () => {
     // console.log("Clicked Next");  // Clicked Next
     currentImg++;
+    clearTimeout(timeout);
     updateImg();
 })
+
+prevElement.addEventListener("click", () => {
+    // console.log("Clicked Previous"); // Clicked Previous
+    currentImg--;
+    clearTimeout(timeout);
+    updateImg();
+})
+
+updateImg();
 
 function updateImg() {
     if(currentImg > imgsElement.length) {
         currentImg = 1;
+    } else if(currentImg < 1) {
+        currentImg = imgsElement.length;
     }
-    imageContainerElement.style.transform =  `translateX(-${currentImg - 1 * 500}px)`;
+    imageContainerElement.style.transform =  `translateX(-${(currentImg - 1 )* 500}px)`;
+    timeout = setTimeout(() => {
+        currentImg++;
+        updateImg();
+    }, 3000)
 };
-
-
-prevElement.addEventListener("click", () => {
-    // console.log("Clicked Previous"); // Clicked Previous
-})
